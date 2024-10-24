8b00111f711103228910db92b0d1c678919d311c
dell <dell@DESKTOP-JE810N7>
2024-10-24 16:12:37 +0530

    add on

--- core_java_basics/src/day1/Demo.java
+++ core_java_basics/src/day1/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("java session started");
 		System.out.print("by s.saravanan");
+		System.out.print("seenu");
 	
 		}
 
